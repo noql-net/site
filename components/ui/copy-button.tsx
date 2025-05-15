62b9e02fd917869d6639de5af2799a0c550d4971
IRCF <128790947+ircfspace@users.noreply.github.com>
2025-05-15 11:28:10 +0330

    install and run Prettier to format codebase

--- components/ui/copy-button.tsx
+++ components/ui/copy-button.tsx
@@ -1,14 +1,20 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React from 'react';
 
-export function CopyButton({ textToCopy, contentName }: { textToCopy: string, contentName: string }) {
-  return (
-    <button 
-      onClick={() => navigator.clipboard.writeText(textToCopy)}
-      className="text-green-400 hover:text-green-300 ml-2 text-sm font-medium min-w-[110px] text-center"
-    >
-      Copy {contentName}
-    </button>
-  );
+export function CopyButton({
+    textToCopy,
+    contentName
+}: {
+    textToCopy: string;
+    contentName: string;
+}) {
+    return (
+        <button
+            onClick={() => navigator.clipboard.writeText(textToCopy)}
+            className='text-green-400 hover:text-green-300 ml-2 text-sm font-medium min-w-[110px] text-center'
+        >
+            Copy {contentName}
+        </button>
+    );
 }
